d91e7310b473780f3880403cbe7a7c0cb5c0226a
unknown <Mandeep-dk>
2026-02-27 14:09:14 +0530

    Change the team size

--- frontend/src/components/EventsSection.jsx
+++ frontend/src/components/EventsSection.jsx
@@ -120,7 +120,7 @@ export default function EventsSection({ onEventClick, onRegisterClick }) {
                   </div>
                   <div className="flex items-center text-gray-600">
                     <Users className="w-5 h-5 mr-3 text-[#003d82]" />
-                    <span>Team Size: 2-4 members</span>
+                    <span>Team Size: 3-5 members</span>
                   </div>
                 </div>
 
